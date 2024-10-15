82f4a71aafd35243e61b8854b6a04e079daf0ac6
Chris <chris_ic3@hotmail.com>
2024-10-14 20:32:13 -0500

    feat: refactor variable names and add edit feature

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-user_action_prompt = "Type add, view, or exit:"
-enter_todo_prompt = "Enter a todo:"
+user_action_prompt = "Type add, show, edit, or exit:"
+enter_todo_prompt = "Enter a new todo:"
+todo_number_prompt = "Enter the number of the todo you would like to edit:"
 
-todos = []
+todo_list = []
 
 while True:
     user_action = input(user_action_prompt)
@@ -9,11 +10,21 @@ while True:
 
     match user_action:
         case "add":
-            todo = input(enter_todo_prompt)
-            todos.append(todo)
-        case "view":
-            for item in todos:
+            todo_item = input(enter_todo_prompt)
+            todo_list.append(todo_item)
+        case "show":
+            for item in todo_list:
                 print(item)
+        # TODO: Add case for "edit"
+        #   Use numbers for indexing
+        case "edit":
+            # Cast str input to int
+            todo_number = int(input(todo_number_prompt))
+            # Subtract 1 because the list starts at 0 and the user
+            #   doesn't know that
+            todo_number = todo_number - 1
+            new_todo_item = input(enter_todo_prompt)
+            todo_list[todo_number] = new_todo_item
         case "exit":
             break
 
