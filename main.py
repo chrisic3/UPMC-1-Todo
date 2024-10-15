a6a132667caa822eae2341bdad3f4640db6afde8
Chris <chris_ic3@hotmail.com>
2024-10-14 20:23:57 -0500

    feat: add user input selection
    Refactor initial prompt.
    Use match statement to implement user selection.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-user_prompt = "Enter a to-do:"
+user_action_prompt = "Type add, view, or exit:"
+enter_todo_prompt = "Enter a to-do:"
 
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input(user_action_prompt)
+    match user_action:
+        case "add":
+            todo = input(enter_todo_prompt)
+            todos.append(todo)
+        case "view":
+            print(todos)
+        case "exit":
+            break
+
+print("Bye")
