83a2d648cf6bff15cf7799db4d97a110e4b21ee1
chrisic3 <chris_ic3@hotmail.com>
2024-10-24 18:34:29 +0000

    refactor: change the match statement to if/else to account for better condition checking
    Changed the "add", "edit", and "complete" features to allow the user to input the item or
    number after the menu selection on the same line. This cuts down on extra prompts and
    increases ease of use. The with statement cannot check expressions in the case line, that's
    why it had to change to if/else.

--- main.py
+++ main.py
@@ -7,65 +7,71 @@ while True:
     user_action = input(user_action_prompt)
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo_item = input(enter_todo_prompt) + '\n'
+    if "add" in user_action:
+        # Since the input should be "add {todo item}", 
+        #   split the string to get everything after 
+        #   "add "
+        todo_item = user_action[4:]
 
-            # file = open("todos.txt", 'r')
-            # todo_list = file.readlines()
-            # file.close()
+        # file = open("todos.txt", 'r')
+        # todo_list = file.readlines()
+        # file.close()
 
-            # Changed to with context manager
-            with open("todos.txt", 'r') as file:
-                todo_list = file.readlines()
-            
-            todo_list.append(todo_item)
+        # Changed to with context manager
+        with open("todos.txt", 'r') as file:
+            todo_list = file.readlines()
+        
+        todo_list.append(todo_item)
 
-            with open("todos.txt", 'w') as file:
-                file.writelines(todo_list)
-        case "show":
-            with open("todos.txt", 'r') as file:
-                todo_list = file.readlines()
-            
-            # List comprehension
-            # todo_list = [item.strip('\n') for item in todo_list]
+        with open("todos.txt", 'w') as file:
+            file.writelines(todo_list)
+    elif "show" in user_action:
+        with open("todos.txt", 'r') as file:
+            todo_list = file.readlines()
+        
+        # List comprehension
+        # todo_list = [item.strip('\n') for item in todo_list]
 
-            for index, item in enumerate(todo_list):
-                item = item.strip('\n')
-                row = f"{index + 1}. {item}"
-                print(row)
-        case "edit":
-            # Cast str input to int
-            edit_todo_number = int(input(edit_todo_prompt))
+        for index, item in enumerate(todo_list):
+            item = item.strip('\n')
+            row = f"{index + 1}. {item}"
+            print(row)
+    elif "edit" in user_action:
+        # Same reason for string split as in the "add" feature
+        # Cast str input to int
+        edit_todo_number = int(user_action[5:])
 
-            # Subtract 1 because the list starts at 0 and the user
-            #   doesn't know that
-            edit_todo_number = edit_todo_number - 1
+        # Subtract 1 because the list starts at 0 and the user
+        #   doesn't know that
+        edit_todo_number = edit_todo_number - 1
 
-            with open("todos.txt", 'r') as file:
-                todo_list = file.readlines()
-            
-            new_todo_item = input(enter_todo_prompt)
-            todo_list[edit_todo_number] = new_todo_item + '\n'
+        with open("todos.txt", 'r') as file:
+            todo_list = file.readlines()
+        
+        new_todo_item = input(enter_todo_prompt)
+        todo_list[edit_todo_number] = new_todo_item + '\n'
 
-            with open("todos.txt", 'w') as file:
-                file.writelines(todo_list)
-        case "complete":
-            # Cast str input to int
-            complete_todo_number = int(input(complete_todo_prompt))
+        with open("todos.txt", 'w') as file:
+            file.writelines(todo_list)
+    elif "complete" in user_action:
+        # Same reason for the string split as in the "add" feature
+        # Cast str input to int
+        complete_todo_number = int(user_action[9:])
 
-            with open("todos.txt", 'r') as file:
-                todo_list = file.readlines()
+        with open("todos.txt", 'r') as file:
+            todo_list = file.readlines()
 
-            # Subtract 1 because the list starts at 0 and the user
-            #   doesn't know that. Also store the item to print out later.
-            #   Also remove the new line character.
-            removed_todo = todo_list.pop(complete_todo_number - 1).strip('\n')
-            print(f'The item "{removed_todo}" was removed from the list.')
+        # Subtract 1 because the list starts at 0 and the user
+        #   doesn't know that. Also store the item to print out later.
+        #   Also remove the new line character.
+        removed_todo = todo_list.pop(complete_todo_number - 1).strip('\n')
+        print(f'The item "{removed_todo}" was removed from the list.')
 
-            with open("todos.txt", 'w') as file:
-                file.writelines(todo_list)
-        case "exit":
-            break
+        with open("todos.txt", 'w') as file:
+            file.writelines(todo_list)
+    elif "exit" in user_action:
+        break
+    else:
+        print("Illegal input")
 
 print("Bye")
