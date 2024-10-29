73f1a6e142d9afeb9b7d3e28f25213afc9000203
chrisic3 <chris_ic3@hotmail.com>
2024-10-29 13:14:26 +0000

    refactor: replacemultiple file reads with a fcustom function

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# Function to open a file and read the lines to a list
+def get_todos():
+    # file = open("todos.txt", 'r')
+    # todo_list = file.readlines()
+    # file.close()
+
+    # Changed to with context manager
+    with open("todos.txt", 'r') as file_local:
+        todo_list_local = file_local.readlines()
+
+    return todo_list_local
+
+
 user_action_prompt = "Type add, show, edit, complete, or exit: "
 enter_todo_prompt = "Enter a new todo: "
 edit_todo_prompt = "Enter the number of the todo you would like to edit: "
@@ -13,21 +26,14 @@ while True:
         #   "add "
         todo_item = user_action[4:]
 
-        # file = open("todos.txt", 'r')
-        # todo_list = file.readlines()
-        # file.close()
-
-        # Changed to with context manager
-        with open("todos.txt", 'r') as file:
-            todo_list = file.readlines()
+        todo_list = get_todos()
         
         todo_list.append(todo_item + '\n')
 
         with open("todos.txt", 'w') as file:
             file.writelines(todo_list)
     elif user_action.startswith("show"):
-        with open("todos.txt", 'r') as file:
-            todo_list = file.readlines()
+        todo_list = get_todos()
         
         # List comprehension
         # todo_list = [item.strip('\n') for item in todo_list]
@@ -46,8 +52,7 @@ while True:
             #   doesn't know that
             edit_todo_number = edit_todo_number - 1
 
-            with open("todos.txt", 'r') as file:
-                todo_list = file.readlines()
+            todo_list = get_todos()
             
             new_todo_item = input(enter_todo_prompt)
             todo_list[edit_todo_number] = new_todo_item + '\n'
@@ -63,8 +68,7 @@ while True:
             # Cast str input to int
             complete_todo_number = int(user_action[9:])
 
-            with open("todos.txt", 'r') as file:
-                todo_list = file.readlines()
+            todo_list = get_todos()
 
             # Subtract 1 because the list starts at 0 and the user
             #   doesn't know that. Also store the item to print out later.
