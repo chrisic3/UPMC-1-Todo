97169f8fa4866e5d9585ab083b5145a0197401bb
Chris <chris_ic3@hotmail.com>
2024-11-04 20:56:40 -0600

    refactor: add docstrings to explain the functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Function to open a file and read the lines to a list
 def get_todos(filepath="todos.txt"):
+    """ Read a list of items from a text file. """
     # file = open("todos.txt", 'r')
     # todo_list = file.readlines()
     # file.close()
@@ -13,6 +14,7 @@ def get_todos(filepath="todos.txt"):
 
 # Function to open a file and read the lines to a list
 def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write a list of items to a text file. """
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
